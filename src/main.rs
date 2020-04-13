a1da0aa5f9646120c586d78c0ef7210f84a5dcbf
yusei-wy <yusei.kasa@gmail.com>
2020-04-13 14:30:22 +0900

    Miscellaneouse flags

--- src/main.rs
+++ src/main.rs
@@ -22,8 +22,9 @@ impl StdinRawMode {
 
         let mut termios = Termios::from_fd(self.fd).unwrap();
 
-        termios.c_iflag &= !(ICRNL | IXON);
+        termios.c_iflag &= !(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
         termios.c_oflag &= !(OPOST);
+        termios.c_cflag &= !(CS8);
         termios.c_lflag &= !(ECHO | ICANON | IEXTEN | ISIG);
 
         tcsetattr(self.fd, TCSAFLUSH, &termios).unwrap();
