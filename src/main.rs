0ed243538669a70fcc2338e5a131f3cb0533ff1b
yusei-wy <yusei.kasa@gmail.com>
2020-07-03 19:16:58 +0900

    Turn off echoing

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,50 @@
 use kirocode::Result;
 
 use std::io::{self, Read};
+use std::os::unix::io::AsRawFd;
 
-fn main() {
-    let mut stdin = io::stdin();
-    loop {
-        match read_byte(&mut stdin) {
-            Ok(ob) => {
-                if let Some(b) = ob {
-                    let c = b as char;
-                    if c == 'q' {
-                        break;
-                    }
-                }
-            },
-            Err(e) => print!("{}", e),
-        }
+struct StdinRawMode {
+    stdin: io::Stdin,
+}
+
+impl StdinRawMode {
+    fn new() -> Result<Self> {
+        use termios::*;
+        let stdin = io::stdin();
+        let fd = stdin.as_raw_fd();
+        let mut termios = Termios::from_fd(fd)?;
+
+        // C/C++ でビットの NOT 演算子は '~'
+        termios.c_lflag &= !(ECHO);
+
+        Ok(Self { stdin })
+    }
+
+    fn read_byte(&mut self) -> Result<Option<u8>> {
+        let mut one_byte: [u8; 1] = [0];
+        Ok(if self.stdin.read(&mut one_byte)? == 0 {
+            None
+        } else {
+            Some(one_byte[0])
+        })
     }
 }
 
-fn read_byte(stdin: &mut io::Stdin) -> Result<Option<u8>> {
-    let mut one_byte: [u8; 1] = [0];
-    Ok(if stdin.read(&mut one_byte)? == 0 {
-        None
-    } else {
-        Some(one_byte[0])
-    })
+fn main() {
+    match StdinRawMode::new() {
+        Ok(mut input) => loop {
+            match input.read_byte() {
+                Ok(ob) => {
+                    if let Some(b) = ob {
+                        let c = b as char;
+                        if c == 'q' {
+                            break;
+                        }
+                    }
+                }
+                Err(e) => print!("{}", e),
+            }
+        },
+        Err(err) => eprintln!("{}", err),
+    };
 }
