cc706b8998d25052177226edd4248bcf77b98242
yusei-wy <yusei.kasa@gmail.com>
2020-07-05 11:49:33 +0900

    add: 恐らくすでに off であったり, モダンターミナルでは適用されないが慣習にしたがってフラグを off に

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,9 @@ impl StdinRawMode {
         let org = termios;
 
         // C/C++ でビットの NOT 演算子は '~'
-        termios.c_iflag &= !(ICRNL | IXON);
+        termios.c_iflag &= !(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
         termios.c_oflag &= !(OPOST);
+        termios.c_cflag &= !(CS8);
         termios.c_lflag &= !(ECHO | ICANON | IEXTEN | ISIG);
 
         tcsetattr(fd, TCSAFLUSH, &termios)?;
